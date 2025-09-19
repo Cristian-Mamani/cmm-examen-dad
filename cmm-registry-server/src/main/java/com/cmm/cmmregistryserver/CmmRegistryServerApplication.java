88a5cd48547225d913b51a6621810c33fcde2dc9
markmercado1 <markemerson1245@gmail.com>
2025-09-18 21:27:07 -0500

    cambios

---
+++ cmm-registry-server/src/main/java/com/cmm/cmmregistryserver/CmmRegistryServerApplication.java
@@ -0,0 +1,13 @@
+package com.cmm.cmmregistryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmmRegistryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmmRegistryServerApplication.class, args);
+    }
+
+}
---
+++ cmm-registry-server/src/test/java/com/cmm/cmmregistryserver/CmmRegistryServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cmm.cmmregistryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootTest
+class CmmRegistryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
